6150c48ac1ad84952a0d820f6eeda20e5e5f5cad
cjang3285 <cjang6199@gmail.com>
2026-03-01 10:43:14 +0900

    clean up

--- backend/routes/releaseRoutes.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import { getReleases, downloadRelease } from '../controllers/releaseController.js';
-
-const router = express.Router();
-
-router.get('/', getReleases);
-router.get('/download/:filename', downloadRelease);
-
-export default router;
